28d9ca389e59c8f08f29dbab41b2af4cd678be87
Marina Drugina <druginamarina@gmail.com>
2022-11-08 22:01:22 +0300

    modified script and tests

--- src/js/character.js
+++ src/js/character.js
@@ -1,14 +1,20 @@
 export default class Character {
   constructor(name, type, attack, defence, health = 100, level = 1) {
-    if ((name.length >= 2 && name.length <= 10) && (Character.types.includes(type))) {
-      this.name = name;
-      this.type = type;
-      this.health = health;
-      this.level = level;
-      this.attack = attack;
-      this.defence = defence;
-    } else {
-      throw new Error('Неверные данные');
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
+
+    if ((name.length <= 2 || name.length >= 10) && Character.types.includes(type)) {
+      throw new Error('Name length must be in [2;10] interval!');
+    }
+    if ((name.length >= 2 && name.length <= 10) && !Character.types.includes(type)) {
+      throw new Error('Not valid character type!');
+    }
+    if ((name.length <= 2 || name.length >= 10) && !Character.types.includes(type)) {
+      throw new Error('Not valid character name & type!');
     }
   }
 }
